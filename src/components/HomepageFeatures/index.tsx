4a3b5cce1f100dbb6db9e67c6d1237b8fa63154c
chientrm <chientrm@gmail.com>
2022-09-15 03:31:40 +0000

    init project

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: "Worldwide servers",
+    Svg: require("@site/static/img/globe-eurafrica.svg").default,
+    description: (
+      <>
+        34 dedicated server regions over the world to make sure homies have a
+        great time together.
+      </>
+    ),
+  },
+  {
+    title: "Money saving",
+    Svg: require("@site/static/img/chovynz_Money_Bag_Icon.svg").default,
+    description: (
+      <>
+        Pay for running server per minute. Quickly deploy/terminate/resume game
+        server.
+      </>
+    ),
+  },
+  {
+    title: "Integrated with DST",
+    Svg: require("@site/static/img/publicdomainq-native.svg").default,
+    description: (
+      <>
+        Realtime log tracing, quickly pause/resume/rollback/kick/vote directly
+        from the web. Built for mod-lovers. Anti-griefers system.
+      </>
+    ),
+  },
+];
+
+function Feature({ title, Svg, description }: FeatureItem) {
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+
+import styles from "./index.module.css";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="https://gogetfunding.com/dst-server/"
+            target="_blank"
+          >
+            Fund us - 5min ⏱
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Launching Don't Starve Together dedicated server at any regions in the world. Join mailing list for latest news and special offers."
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
